88df12c1b9d472f35f104d7ed3d9a8022c61d5f7
Switch <q547550831@outlook.com>
2017-11-12 20:17:52 +0800

    update chapter7

---
+++ src/main/java/com/switchvov/chapter7/item41/SetList.java
@@ -0,0 +1,22 @@
+package com.switchvov.chapter7.item41;// What does this program print? - Page 194
+
+import java.util.*;
+
+public class SetList {
+    public static void main(String[] args) {
+        Set<Integer> set = new TreeSet<>();
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = -3; i < 3; i++) {
+            set.add(i);
+            list.add(i);
+        }
+
+        for (int i = 0; i < 3; i++) {
+            set.remove(i);
+            list.remove(i);
+        }
+
+        System.out.println(set + " " + list);
+    }
+}
